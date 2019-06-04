754fe842cda35655871542eb5e735e95db8d1e5b
Nicole Howard <nhoward2@zagmail.gonzaga.edu>
2019-06-03 17:36:47 -0700

    init sdk setup

---
+++ spotify-ios-quick-start/BridgeViewController.h
@@ -0,0 +1,17 @@
+//
+//  BridgeViewController.h
+//  spotify-ios-quick-start
+//
+//  Created by Nicole Howard on 6/3/19.
+//  Copyright © 2019 Nicole Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BridgeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
