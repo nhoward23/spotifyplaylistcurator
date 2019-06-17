26a306fd1ff2b319d37c07633d754acbf738e1a3
Nicole Howard <nhoward2@zagmail.gonzaga.edu>
2019-06-17 13:39:44 -0700

    create playlist and song objects

---
+++ spotify-ios-quick-start/Playlist.swift
@@ -0,0 +1,16 @@
+//
+//  Playlist.swift
+//  spotify-ios-quick-start
+//
+//  Created by Nicole Howard on 6/16/19.
+//  Copyright © 2019 Nicole Howard. All rights reserved.
+//
+
+import Foundation
+
+
+struct Playlist {
+    
+    var songs: [Song]
+    
+}
---
+++ spotify-ios-quick-start/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  spotify-ios-quick-start
+//
+//  Created by Nicole Howard on 6/16/19.
+//  Copyright © 2019 Nicole Howard. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    
+    var id: String
+    var title: String
+    var artist: String
+    var album: String
+    
+}
